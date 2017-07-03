4f31260484b3eec842ae0b7ef5f086e1e279f2ca
jiseung <anna.jiseung@gmail.com>
2017-07-03 16:19:07 +0000

    added proxy-app tag for clamr

--- packages/clamr/package.py
+++ packages/clamr/package.py
@@ -41,13 +41,11 @@ from spack import *
 import inspect
 
 class Clamr(CMakePackage):
-    """The CLAMR code is a cell-based adaptive mesh refinement (AMR) mini-app developed as a testbed for hybrid algorithm development using MPI and OpenCL GPU code.
-
-proxy-app
-ecp-proxy-app"""
+    """The CLAMR code is a cell-based adaptive mesh refinement (AMR) mini-app developed as a testbed for hybrid algorithm development using MPI and OpenCL GPU code."""
 
     homepage = "https://github.com/lanl/CLAMR"
     url      = ""
+    tags     = ['proxy-app']
 
     version('master', git='https://github.com/lanl/CLAMR.git')
 
