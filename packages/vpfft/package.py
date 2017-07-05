1b85785257d2e8ea135172f184faf4c0daf0a867
akhilpatlolla <patlolla.akhil@gmail.com>
2017-07-05 20:04:32 +0000

    Added VPFFT

---
+++ packages/vpfft/package.py
@@ -0,0 +1,69 @@
+##############################################################################
+# Copyright (c) 2013-2016, Lawrence Livermore National Security, LLC.
+# Produced at the Lawrence Livermore National Laboratory.
+#
+# This file is part of Spack.
+# Created by Todd Gamblin, tgamblin@llnl.gov, All rights reserved.
+# LLNL-CODE-647188
+#
+# For details, see https://github.com/llnl/spack
+# Please also see the NOTICE and LICENSE files for our notice and the LGPL.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License (as
+# published by the Free Software Foundation) version 2.1, February 1999.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms and
+# conditions of the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+##############################################################################
+#
+# This is a template package file for Spack.  We've put "FIXME"
+# next to all the things you'll want to change. Once you've handled
+# them, you can save this file and test your package like this:
+#
+#     spack install vpfft
+#
+# You can edit this file again by typing:
+#
+#     spack edit vpfft
+#
+# See the Spack documentation for more information on packaging.
+# If you submit this package back to Spack as a pull request,
+# please first remove this boilerplate and all FIXME comments.
+#
+from spack import *
+
+
+class Vpfft(CMakePackage):
+    """ Proxy Application. VPFFT is an implementation of a mesoscale micromechanical materials 
+    model. By solving the viscoplasticity model, VPFFT simulates the evolution
+     of a material under deformation. The solution time to the viscoplasticity 
+     model, described by a set of partial differential equations, is 
+     significantly reduced by the application of Fast Fourier Transform 
+     in the VPFFT algorithm.
+
+    tags : proxy-app ecp-proxy-app
+     """
+
+    tag = ['proxy-app','ecp-proxy-app']
+    homepage = "http://www.exmatex.org/vpfft.html"
+    url      = "https://github.com/exmatex/VPFFT/archive/master.tar.gz"
+
+    # FIXME: Add proper versions and checksums here.
+    # version('1.2.3', '0123456789abcdef0123456789abcdef')
+
+    # FIXME: Add dependencies if required.
+    # depends_on('foo')
+
+    def cmake_args(self):
+        # FIXME: Add arguments other than
+        # FIXME: CMAKE_INSTALL_PREFIX and CMAKE_BUILD_TYPE
+        # FIXME: If not needed delete this function
+        args = []
+        return args
