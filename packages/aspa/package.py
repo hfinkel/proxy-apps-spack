ac9116f4d8b259b2c12f08e26856f6bed53cc49b
akhilpatlolla <patlolla.akhil@gmail.com>
2017-07-12 21:30:18 +0000

    PEP 8 Stype Updated

--- packages/aspa/package.py
+++ packages/aspa/package.py
@@ -28,20 +28,22 @@ import glob
 
 class Aspa(MakefilePackage):
     """A fundamental premise in ExMatEx is that scale-bridging performed in
-    heterogeneous MPMD materials science simulations will place important
-    demands upon the exascale ecosystem that need to be identified and quantified.
-    tags = proxy-app
+        heterogeneous MPMD materials science simulations will place important
+        demands upon the exascale ecosystem that need to be identified and
+        quantified.
+        tags = proxy-app
     """
     tags = ['proxy-app']
     homepage = "http://www.exmatex.org/aspa.html"
     url = "https://github.com/exmatex/ASPA/archive/master.tar.gz"
 
-    version('master', git='https://github.com/exmatex/ASPA.git',description='master')
-    variant('mpi',default=True,description='Build with MPI Support')
+    version('master', git='https://github.com/exmatex/ASPA.git',
+            description='master')
+    variant('mpi', default=True, description='Build with MPI Support')
 
     depends_on('lapack')
     depends_on('blas')
-    depends_on('mpi',when='+mpi')
+    depends_on('mpi', when='+mpi')
     depends_on('hdf5')
 
     @property
@@ -49,7 +51,9 @@ class Aspa(MakefilePackage):
         targets = [
             '--directory=exec',
             '--file=Makefile',
-            'LIBS={0} {1} {2}'.format(self.spec['lapack'].libs.ld_flags,self.spec['blas'].libs.ld_flags,self.spec['hdf5'].libs.ld_flags),
+            'LIBS={0} {1} {2}'.format(self.spec['lapack'].libs.ld_flags,
+                                      self.spec['blas'].libs.ld_flags,
+                                      self.spec['hdf5'].libs.ld_flags),
             'CXX={0}'.format(self.spec['mpi'].mpicxx)
         ]
         return targets
@@ -58,11 +62,11 @@ class Aspa(MakefilePackage):
         mkdirp(prefix.bin)
         mkdirp(prefix.doc)
         mkdirp(prefix.input)
-        install('exec/aspa',prefix.bin)
-        install('exec/README',prefix.doc)
-        install('exec/aspa.inp',prefix.input)
-        install('exec/kriging_model_centers.txt',prefix.input)
-        install('exec/point_data.txt',prefix.input)
-        install('exec/value_data.txt',prefix.input)
+        install('exec/aspa', prefix.bin)
+        install('exec/README', prefix.doc)
+        install('exec/aspa.inp', prefix.input)
+        install('exec/kriging_model_centers.txt', prefix.input)
+        install('exec/point_data.txt', prefix.input)
+        install('exec/value_data.txt', prefix.input)
         for files in glob.glob('doc/*.*'):
             install(files, prefix.doc)
