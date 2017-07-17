ebfbcb7f5f13a233e4590461fb33c83805c10de8
akhilpatlolla <patlolla.akhil@gmail.com>
2017-07-17 21:32:23 +0000

    Updated

--- packages/mcb/package.py
+++ packages/mcb/package.py
@@ -22,53 +22,52 @@
 # License along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install mcb
-#
-# You can edit this file again by typing:
-#
-#     spack edit mcb
-#
-# See the Spack documentation for more information on packaging.
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
 from spack import *
-import os
+import tarfile
+import glob
 
-class Mcb(Package):
-    """The "Monte Carlo Benchmark" (MCB) is intended for use in exploring 
-    the computational performance of Monte Carlo algorithms on parallel architectures
 
-    tags : proxy-app ecp-proxy-app"""
+class Mcb(MakefilePackage):
+    """The "Monte Carlo Benchmark" (MCB) is intended for use in exploring
+        the computational performance of Monte Carlo algorithms on parallel
+        architectures.
+        tags = proxy-app
+    """
 
-    tags = ['exp-proxy-app','proxy-app']
+    tags = ['proxy-app']
     homepage = "https://codesign.llnl.gov/mcb.php"
-    url      = "https://codesign.llnl.gov/downloads/mcb-20130723.tar.gz"
+    url = "https://codesign.llnl.gov/downloads/mcb-20130723.tar.gz"
 
     version('20130723', 'ed9c97edb45c8918184b4eba280bd884')
+    depends_on('mpi')
 
-    # FIXME: Add dependencies if required.
-    depends_on('openmpi')
+    build_directory = 'src'
+    
+    def edit(self, spec, prefix):
+        for fname in glob.glob('*.*'):
+            if (fname.endswith("tgz")):
+                tar = tarfile.open(fname, "r:gz")
+                tar.extractall()
+                tar.close()
 
-    def install(self,spec,prefix):
-        os.system('./build-linux-x86_64')
-        
+        with working_dir('src'):
+            filter_file(r'^BOOST_PREFIX\s*=.*',
+                        'BOOST_PREFIX = ../boost_headers/boost',
+                        'Makefile')
+            filter_file(r'^CXX\s*=.*', 'CXX = %s' % self.spec['mpi'].mpicc,
+                        'Makefile')
+            filter_file(r'^OPENMPFLAG\s*=.*',
+                        'OPENMPFLAG = %s' % self.compiler.openmp_flag,
+                        'Makefile')
+            filter_file(r'^MPI_INCLUDE\s*=.*',
+                        'MPI_INCLUDE = %s' % self.spec['mpi'].prefix.include,
+                        'Makefile')
+            filter_file(r'^CXXFLAGS=\s*=.*',
+                        '',
+                        'Makefile')
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+    def install(self, spec, prefix):
+        install_tree('src/Documentation', prefix.doc)
+        mkdirp(prefix.doc.install)
+        install('README', prefix.doc.install)
+        install('src/README', prefix.doc)
