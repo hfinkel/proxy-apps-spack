0fa763c04ec72a712b5655a3873296fc879ac228
akhilpatlolla <patlolla.akhil@gmail.com>
2017-07-20 17:21:38 +0000

    Updated Changes on  MCB

--- packages/mcb/package.py
+++ packages/mcb/package.py
@@ -25,6 +25,7 @@
 from spack import *
 import tarfile
 import glob
+from distutils.dir_util import copy_tree
 
 
 class Mcb(MakefilePackage):
@@ -39,19 +40,21 @@ class Mcb(MakefilePackage):
 
     version('20130723', 'ed9c97edb45c8918184b4eba280bd884')
     depends_on('mpi')
+    depends_on('boost')
 
     build_directory = 'src'
-    
     def edit(self, spec, prefix):
         for fname in glob.glob('*.*'):
             if (fname.endswith("tgz")):
                 tar = tarfile.open(fname, "r:gz")
                 tar.extractall()
                 tar.close()
-
         with working_dir('src'):
-            filter_file(r'^BOOST_PREFIX\s*=.*',
-                        'BOOST_PREFIX = ../boost_headers/boost/',
+            filter_file(r'^BOOST_INCLUDE\s*=.*',
+                        'BOOST_INCLUDE = -I{0} '.format(self.spec['boost'].prefix.include.boost),
+                        'Makefile')
+            filter_file(r'^BOOST_LIB\s*=.*',
+                        'BOOST_LIB = -L{0} -Wl,-rpath {0}'.format(self.spec['boost'].prefix.lib),
                         'Makefile')
             filter_file(r'^CXX\s*=.*', 'CXX = %s' % self.spec['mpi'].mpicc,
                         'Makefile')
@@ -59,11 +62,12 @@ class Mcb(MakefilePackage):
                         'OPENMPFLAG = %s' % self.compiler.openmp_flag,
                         'Makefile')
             filter_file(r'^MPI_INCLUDE\s*=.*',
-                        'MPI_INCLUDE = %s' % self.spec['mpi'].prefix.include,
+                        'MPI_INCLUDE = -I%s' % self.spec['mpi'].prefix.include,
                         'Makefile')
-            filter_file(r'^CXXFLAGS=\s*=.*',
-                        'CXXFLAGS = -O2 -xHost $(CXXDEFINES) $(OPENMPFLAG)',
+            filter_file(r'^CXXFLAGS\s*=.*',
+                        'CXXFLAGS = -O2 -xHost $(CXXDEFINES) $(OPENMPFLAG) -compiler gcc',
                         'Makefile')
+
 
     def install(self, spec, prefix):
         install_tree('src/Documentation', prefix.doc)
