0cecb96c00f27a2ba77cdefa0dc95f2dba45c78c
jiseung <anna.jiseung@gmail.com>
2017-07-21 14:32:33 +0000

    functional package

--- packages/minigmg/package.py
+++ packages/minigmg/package.py
@@ -22,39 +22,44 @@
 # License along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install minigmg
-#
-# You can edit this file again by typing:
-#
-#     spack edit minigmg
-#
-# See the Spack documentation for more information on packaging.
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
 from spack import *
+import glob
 
 
 class Minigmg(Package):
-    """miniGMG is a compact benchmark for understanding the performance challenges associated with geometric multigrid solvers found in applications built from AMR MG frameworks like CHOMBO or BoxLib when running on modern multi- and manycore-based supercomputers. It includes both productive reference examples as well as highly-optimized implementations for CPUs and GPUs. It is sufficiently general that it has been used to evaluate a broad range of research topics including PGAS programming models and algorithmic tradeoffs inherit in multigrid. miniGMG was developed under the CACHE Joint Math-CS Institute.
+    """miniGMG is a compact benchmark for understanding the performance
+    challenges associated with geometric multigrid solvers
+    found in applications built from AMR MG frameworks
+    like CHOMBO or BoxLib when running
+    on modern multi- and manycore-based supercomputers.
+    It includes both productive reference examples as well as
+    highly-optimized implementations for CPUs and GPUs.
+    It is sufficiently general that it has been used to evaluate
+    a broad range of research topics including PGAS programming models
+    and algorithmic tradeoffs inherit in multigrid. miniGMG was developed
+    under the CACHE Joint Math-CS Institute.
 
-Note, miniGMG code has been supersceded by HPGMG. """
+    Note, miniGMG code has been supersceded by HPGMG. """
 
     homepage = "http://crd.lbl.gov/departments/computer-science/PAR/research/previous-projects/miniGMG/"
     url      = "http://crd.lbl.gov/assets/Uploads/FTG/Projects/miniGMG/miniGMG.tar.gz"
 
+    version('master', '975a2a118403fc0024b5e04cef280e95')
     version('cuda', url='http://crd.lbl.gov/assets/Uploads/FTG/Projects/miniGMG/miniGMG.cuda.tar.gz')
 
     depends_on('mpi')
-    depends_on('openmp')
     depends_on('cuda', when='@cuda')
 
+    phases = ['build', 'install']
+
+    def build(self, spec, prefix):
+        cc = Executable(spec['mpi'].mpicc)
+        cc('-O3', '{0}'.format(self.compiler.openmp_flag), 'miniGMG.c', 'mg.c', 'box.c', 'solver.c', 'operators.ompif.c', 'timer.x86.c', '-D__MPI', '-D__COLLABORATIVE_THREADING=6', '-D__TEST_MG_CONVERGENCE', '-D__PRINT_NORM', '-D__USE_BICGSTAB', '-o', 'run.miniGMG', '-lm')
+
     def install(self, spec, prefix):
-        # FIXME: Unknown build system
-        make()
-        make('install')
+        mkdir(prefix.bin)
+        install('run.miniGMG', prefix.bin)
+        mkdir(prefix.jobs)
+        files = glob.glob(join_path('./', 'job*'))
+        for f in files:
+            install(f, prefix.jobs)
