98e5ee26d8f52a5b448ee56cd94e29e39a36c49d
jiseung <anna.jiseung@gmail.com>
2017-08-08 19:52:18 +0000

    final VPFFT

--- packages/vpfft/package.py
+++ packages/vpfft/package.py
@@ -23,35 +23,45 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 ##############################################################################
 from spack import *
-import shutil
 
 
-class Vpfft(CMakePackage):
-    """ Proxy Application. VPFFT is an implementation of a mesoscale 
-    micromechanical materials model. By solving the viscoplasticity 
+class Vpfft(MakefilePackage):
+    """Proxy Application. VPFFT is an implementation of a mesoscale
+    micromechanical materials model. By solving the viscoplasticity
     model, VPFFT simulates the evolution of a material under deformation.
     The solution time to the viscoplasticity model, described by a set
     of partial differential equations, is significantly reduced by the
     application of Fast Fourier Transform in the VPFFT algorithm.
     """
 
-    tag = ['proxy-app']
     homepage = "http://www.exmatex.org/vpfft.html"
     url      = "https://github.com/exmatex/VPFFT/archive/master.tar.gz"
+    tag      = ['proxy-app']
 
-    version('master', git='https://github.com/exmatex/VPFFT.git')
+    version('develop', git='https://github.com/exmatex/VPFFT.git')
 
-    depends_on('cmake@2.4:', type='build')
     depends_on('eigen')
     depends_on('fftw')
+    depends_on('mpi')
 
-    phases = ['edit', 'cmake', 'build', 'install']
+    @property
+    def build_targets(self):
+        targets = [
+            "--file=Makefile.make",
+            "EIGEN_PATH={0}".format(
+                join_path(
+                    self.spec['eigen'].prefix.include,
+                    'eigen{0}'.format(
+                        self.spec['eigen'].version.up_to(1)))),
+            "FFTW_PATH={0}".format(self.spec['fftw'].prefix),
+            "CC={0}".format(self.spec['mpi'].mpicxx)
+        ]
+        return targets
 
-    def edit(self, spec, prefix):
-        with working_dir(self.stage.source_path):
-            shutil.move('CMakeBuild/CMakeLists.txt.example', 'CMakeLists.txt')
-            cmake_file = FileFilter('CMakeLists.txt')
-            cmake_file.filter('set.*CMAKE_C_COMPILER.*', 'set(CMAKE_C_COMPILER "{0}")'.format(self.spec['mpi'].mpicc))
-            cmake_file.filter('set.*CMAKE_CXX_COMPILER.*', 'set(CMAKE_CXX_COMPILER "{0}")'.format(self.spec['mpi'].mpicxx))
-            cmake_file.filter('set.*EIGEN_PATH.*', 'set(EIGEN_PATH "{0}/include/eigen3")'.format(self.spec['eigen'].prefix))
-            cmake_file.filter('set.*FFTW_PATH.*', 'set(FFTW_PATH "{0}/include")'.format(self.spec['fftw'].prefix))
+    def install(self, spec, prefix):
+        mkdirp(prefix.bin)
+        install('VPFFT++', prefix.bin)
+        install('README.md', prefix)
+        install('README.make', prefix)
+        install('README-license.txt', prefix)
+        install_tree('docs', prefix.docs)
